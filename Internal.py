c40450b3d82bb3e04af9dc0df439be33bebca01d
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-19 14:21:16 +1200

    ask the user what do do

--- Internal.py
+++ Internal.py
@@ -10,11 +10,15 @@ def print_all():
     db.close()
 def print_legend_rare():
     cursor = db.cursor()
-    sql = 'SELECT * FROM Cat WHERE Cat_Rarity = "Legend Rare;'
+    sql = 'SELECT * FROM Cat WHERE Cat_Rarity = "Legend Rare";'
     cursor.execute(sql)
     result = cursor.fetchall()
     for Cat in result:
         print(f"{Cat[0]:<25}{Cat[1]:<15}{Cat[2]:<15}{Cat[4]:<15}{Cat[5]:<15}")
     db.close()
-
-
+while true:
+    print("1: Print all data")
+    print("2: Print legend rare only")
+    print("3: exit")
+    user_input = input("What would you like to do")
+    
